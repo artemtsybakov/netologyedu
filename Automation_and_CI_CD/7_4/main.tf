ae1e34818172ca7358e0ed6f81a302663179b36c
Artem Tsybakov <artem.cybakov@gmail.com>
2023-09-04 21:29:31 +0600

    kjh

--- Automation_and_CI_CD/7_4/main.tf
+++ Automation_and_CI_CD/7_4/main.tf
@@ -7,13 +7,13 @@ terraform {
   required_version = ">= 0.13"
 }
 
-#variable "yandex_cloud_token" {
-#  type = string
-#  description = "Введите секретный токен от yandex_cloud"
-#}
+variable "yandex_cloud_token" {
+  type = string
+  description = "Введите секретный токен от yandex_cloud"
+}
 
 provider "yandex" {
-  #token     = var.yandex_cloud_token #секретные данные должны быть в сохранности!! Никогда не выкладывайте токен в публичный доступ.
+  token     = var.yandex_cloud_token #секретные данные должны быть в сохранности!! Никогда не выкладывайте токен в публичный доступ.
   cloud_id  = "b1gnjsf5pkse1jbl7obt"
   folder_id = "b1gagbi14br3h73itmd1"
   zone      = "ru-central1-a"
@@ -21,25 +21,31 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "vm-1" {
   name = "terraform1"
+  platform_id = "standard-v1"
 
   resources {
+    core_fraction = 5
     cores  = 2
     memory = 2
   }
-
+  
+  scheduling_policy {
+    preemptible = true
+}
   boot_disk {
     initialize_params {
-      image_id = "fd87kbts7j40q5b9rpjr"
+      image_id = "fd8g5aftj139tv8u2mo1"
+      size = 8
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
     nat       = true
   }
 
   metadata = {
-    user-data = "${file("/home/artem/meta.txt")}"
+    user-data = "${file("/home/artem/meta.yaml")}"
   }
 
 }
@@ -49,7 +55,7 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-  zone           = "ru-central1-b"
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
