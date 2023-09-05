1e548b238297ed6c45217a6987460224cacd5b70
Artem Tsybakov <artem.cybakov@gmail.com>
2023-09-05 23:30:26 +0600

    2 задание

--- Automation_and_CI_CD/7_4/3/main.tf
+++ Automation_and_CI_CD/7_4/3/main.tf
@@ -7,21 +7,21 @@ terraform {
   required_version = ">= 0.13"
 }
 
-variable "yandex_cloud_token" {
-  type = string
-  description = "Введите секретный токен от yandex_cloud"
-}
+#variable "yandex_cloud_token" {
+#  type = string
+#  description = "Введите секретный токен от yandex_cloud"
+#}
 
 provider "yandex" {
-  token     = var.yandex_cloud_token #секретные данные должны быть в сохранности!! Никогда не выкладывайте токен в публичный доступ.
+  #секретные данные должны быть в сохранности!! Никогда не выкладывайте токен в публичный доступ.
   cloud_id  = "b1gnjsf5pkse1jbl7obt"
   folder_id = "b1gagbi14br3h73itmd1"
   zone      = "ru-central1-a"
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
-  hostname = "terraformlab"
+  name = "terraform3"
+  hostname = "terraformlab3"
   platform_id = "standard-v2"
 
   resources {
@@ -32,7 +32,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   scheduling_policy {
     preemptible = true
-  }
+}
   boot_disk {
     initialize_params {
       image_id = "fd8g5aftj139tv8u2mo1"
@@ -50,39 +50,6 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
 }
-
-resource "yandex_compute_instance" "vm-2" {
-  name = "terraform2"
-  hostname = "terraformlab2"
-  platform_id = "standard-v2"
-
-  resources {
-    core_fraction = 5
-    cores  = 2
-    memory = 2
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-  boot_disk {
-    initialize_params {
-      image_id = "fd8g5aftj139tv8u2mo1"
-      size = 8
-    }
-  }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
-    nat       = true
-  }
-
-  metadata = {
-    user-data = "${file("/home/artem/meta.yaml")}"
-  }
-
-}
-
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
 }
@@ -99,11 +66,4 @@ output "internal_ip_address_vm_1" {
 }
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-}
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
